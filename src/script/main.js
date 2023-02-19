cbd0f6b91a578633646d5bf5b2c28b89d3700d79
Aleksandr Pashkevych <alekspashkevych@gmail.com>
2023-02-19 13:24:59 +0200

    feat added mobile header

--- src/script/main.js
+++ src/script/main.js
@@ -1,6 +1,9 @@
 const column = document.querySelectorAll(".emphasize");
 const btn = document.querySelectorAll(".business__btn");
 
+const burgerMenuIcon = document.querySelector(".header__burger");
+const closeIcon = document.querySelector(".burger__close");
+const burgerIcon = document.querySelector(".burger__icon");
 column.forEach((col) => {
   col.classList.remove("on-move");
   col.onmouseover = () => {
@@ -22,3 +25,42 @@ btn.forEach((btn) => {
     alert(`The Button has been clicked at: ${d.toString()}`);
   };
 });
+
+if (burgerMenuIcon) {
+  // burgerMenuIcon.setAttribute(
+  //   "style",
+  //   "background-image: assets/images/burger-icon.jpg"
+  // );
+  const burgerMenu = document.querySelector(".header__list-wrapper");
+  let menuOpen = false;
+  const showCloseBurgerMenuIcon = () => {
+    burgerIcon.classList.add("hide");
+    closeIcon.classList.remove("hide");
+    closeIcon.classList.add("active");
+  };
+  const hideCloseBurgerMenuIcon = () => {
+    closeIcon.classList.remove("active");
+    closeIcon.classList.add("hide");
+    burgerIcon.classList.remove("hide");
+    // burgerIcon.classList.add("active");
+  };
+  burgerMenuIcon.onclick = () => {
+    if (menuOpen == false) {
+      burgerMenu.classList.add("active");
+      showCloseBurgerMenuIcon();
+      document.body.classList.add("lock");
+      menuOpen = true;
+    } else if (menuOpen == true) {
+      burgerMenu.classList.remove("active");
+      document.body.classList.remove("lock");
+      hideCloseBurgerMenuIcon();
+      menuOpen = false;
+    }
+  };
+  burgerMenu.onclick = () => {
+    burgerMenu.classList.remove("active");
+    document.body.classList.remove("lock");
+    hideCloseBurgerMenuIcon();
+    menuOpen = false;
+  };
+}
